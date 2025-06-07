1f14cf5049fcd5493d615bfda88a58d6948603ac
seszele64 <s.szelewicz@outlook.com>
2025-06-07 13:36:55 +0200

    refactor(init): remove unused imports and simplify initialization

--- wrm_pipeline/wrm_pipeline/__init__.py
+++ wrm_pipeline/wrm_pipeline/__init__.py
@@ -1,16 +1,3 @@
-from dagster import Definitions
-from .assets.stations import wrm_stations_raw_data_asset, wrm_stations_processed_asset
-from .sensors.stations_sensor import s3_raw_stations_sensor
-from .resources import s3_resource
-
-print(f"Loading sensors: {s3_raw_stations_sensor.name}")
-print(f"Sensor type: {type(s3_raw_stations_sensor)}")
-
-defs = Definitions(
-    assets=[wrm_stations_raw_data_asset, wrm_stations_processed_asset],
-    sensors=[s3_raw_stations_sensor],
-    resources={"s3_resource": s3_resource}
-)
+from .definitions import defs
 
 __all__ = ["defs"]
-
