213ea7d72a6678aa19af6538eb5b5296cd00d08f
seszele64 <s.szelewicz@outlook.com>
2025-06-08 13:58:33 +0200

    feat(assets): add additional station assets for re-export

--- wrm_pipeline/wrm_pipeline/assets/assets.py
+++ wrm_pipeline/wrm_pipeline/assets/assets.py
@@ -5,9 +5,13 @@ Imports all assets from submodules and re-exports them for convenience.
 
 # Import assets from different modules
 from .stations import wrm_stations_raw_data_asset, wrm_stations_processed_asset
+from .processed_stations import s3_processed_stations_list, wrm_stations_data, wrm_stations_postgres
 
 # Re-export all assets
 __all__ = [
     "wrm_stations_raw_data_asset",
-    "wrm_stations_processed_asset"
+    "wrm_stations_processed_asset",
+    "s3_processed_stations_list",
+    "wrm_stations_data",
+    "wrm_stations_postgres"
 ]
