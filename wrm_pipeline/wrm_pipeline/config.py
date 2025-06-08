924e0a9a74a8256f50b25d6c601ce9ed65647266
seszele64 <s.szelewicz@outlook.com>
2025-06-08 13:58:04 +0200

    feat(config): add PostgreSQL and S3 configuration variables

--- wrm_pipeline/wrm_pipeline/config.py
+++ wrm_pipeline/wrm_pipeline/config.py
@@ -8,9 +8,21 @@ dotenv_path = os.path.join(os.path.dirname(__file__),
 load_dotenv(dotenv_path=dotenv_path)  # .env is in root directory
 
 # s3
+S3_ENDPOINT_URL = os.environ.get('S3_ENDPOINT_URL')
+S3_ACCESS_KEY_ID = os.environ.get('S3_ACCESS_KEY_ID')
+S3_SECRET_ACCESS_KEY = os.environ.get('S3_SECRET_ACCESS_KEY')
+S3_REGION_NAME = os.environ.get('S3_REGION_NAME', None)
+
 HETZNER_ENDPOINT_URL = os.environ.get('HETZNER_ENDPOINT_URL')
 HETZNER_ACCESS_KEY_ID = os.environ.get('HETZNER_ACCESS_KEY_ID')
 HETZNER_SECRET_ACCESS_KEY = os.environ.get('HETZNER_SECRET_ACCESS_KEY')
+
+# PostgreSQL Configuration
+POSTGRES_HOST = os.environ.get('POSTGRES_HOST', 'localhost')
+POSTGRES_PORT = int(os.environ.get('POSTGRES_PORT', 5432))
+POSTGRES_DB = os.environ.get('POSTGRES_DB')
+POSTGRES_USER = os.environ.get('POSTGRES_USER')
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
 
 # --- URLs for data sources ---
 WRM_FAILURES_DATA_URL = "https://www.wroclaw.pl/open-data/39605eb0-8055-4733-bb02-04b96791d36a/WRM_usterki.csv"
