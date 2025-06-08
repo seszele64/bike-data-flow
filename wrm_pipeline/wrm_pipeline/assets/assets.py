0f3c4eb60dcb3cb28de3898a34c62449d0c49438
seszele64 <s.szelewicz@outlook.com>
2025-06-08 17:17:02 +0200

    feat(assets): update asset imports and re-export definitions

--- wrm_pipeline/wrm_pipeline/assets/assets.py
+++ wrm_pipeline/wrm_pipeline/assets/assets.py
@@ -5,7 +5,15 @@ Imports all assets from submodules and re-exports them for convenience.
 
 # Import assets from different modules
 from .stations import wrm_stations_raw_data_asset, wrm_stations_processed_asset
-from .processed_stations import s3_processed_stations_list, wrm_stations_data, wrm_stations_postgres
+from .stations_deduplicated import s3_processed_stations_list, wrm_stations_data
+from .raw_stations import s3_raw_stations_list, wrm_raw_stations_data, wrm_stations_batch_processor, wrm_stations_processing_summary
+from .postgres_assets import (
+    postgres_connection,
+    bike_stations_table,
+    bike_failures_table,
+    load_stations_to_postgres,
+    stations_data_summary
+)
 
 # Re-export all assets
 __all__ = [
@@ -13,5 +21,13 @@ __all__ = [
     "wrm_stations_processed_asset",
     "s3_processed_stations_list",
     "wrm_stations_data",
-    "wrm_stations_postgres"
+    "s3_raw_stations_list",
+    "wrm_raw_stations_data",
+    "wrm_stations_batch_processor",
+    "wrm_stations_processing_summary",
+    "postgres_connection",
+    "bike_stations_table",
+    "bike_failures_table",
+    "load_stations_to_postgres",
+    "stations_data_summary"
 ]
