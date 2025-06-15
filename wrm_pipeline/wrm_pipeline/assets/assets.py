f09aff11630bf90fc0c45585c7794296a2fd9569
seszele64 <s.szelewicz@outlook.com>
2025-06-15 17:07:50 +0200

    refactor(assets): update asset imports and re-export structure

--- wrm_pipeline/wrm_pipeline/assets/assets.py
+++ wrm_pipeline/wrm_pipeline/assets/assets.py
@@ -4,9 +4,14 @@ Imports all assets from submodules and re-exports them for convenience.
 """
 
 # Import assets from different modules
-from .stations import wrm_stations_raw_data_asset, wrm_stations_processed_asset
+from .stations import (
+    wrm_stations_raw_data_asset,
+    wrm_stations_validated_data_asset,
+    wrm_stations_all_processed_asset,
+    wrm_stations_data_asset,
+    wrm_bikes_data_asset
+)
 from .stations_deduplicated import s3_processed_stations_list, wrm_stations_daily_deduplicated
-from .raw_stations import s3_raw_stations_list, wrm_raw_stations_data, wrm_stations_batch_processor, wrm_stations_processing_summary
 from .postgres_assets import (
     postgres_connection,
     bike_stations_table,
@@ -18,13 +23,12 @@ from .postgres_assets import (
 # Re-export all assets
 __all__ = [
     "wrm_stations_raw_data_asset",
-    "wrm_stations_processed_asset",
+    "wrm_stations_validated_data_asset",
+    "wrm_stations_all_processed_asset",
+    "wrm_stations_data_asset",
+    "wrm_bikes_data_asset",
     "s3_processed_stations_list",
     "wrm_stations_daily_deduplicated",
-    "s3_raw_stations_list",
-    "wrm_raw_stations_data",
-    "wrm_stations_batch_processor",
-    "wrm_stations_processing_summary",
     "postgres_connection",
     "bike_stations_table",
     "bike_failures_table",
