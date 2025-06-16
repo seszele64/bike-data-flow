661ebf6f33f9892264330656ed7c01094f9e0af9
seszele64 <s.szelewicz@outlook.com>
2025-06-16 14:45:33 +0200

    feat(resources): add iceberg IO manager and catalog configuration

--- wrm_pipeline/wrm_pipeline/definitions.py
+++ wrm_pipeline/wrm_pipeline/definitions.py
@@ -7,7 +7,8 @@ from .sensors.stations_sensor import s3_raw_stations_sensor
 from .sensors.s3_processed_to_postgres_sensor import s3_processed_stations_sensor
 from .resources import (
     s3_resource,
-    postgres_resource
+    postgres_resource,
+    iceberg_io_manager
 )
     
 # Load environment variables from .env file in parent directory
@@ -21,6 +22,7 @@ defs = Definitions(
     sensors=[s3_raw_stations_sensor, s3_processed_stations_sensor],
     resources={
         "s3_resource": s3_resource,
-        "postgres_resource": postgres_resource
+        "postgres_resource": postgres_resource,
+        "iceberg_io_manager": iceberg_io_manager,
     }
 )
